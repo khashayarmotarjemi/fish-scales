4f6204d903350b2e416885b009666b9ca35e763f
khashayarmotarjemi <khashayar.k.m@gmail.com>
2022-07-10 00:45:35 -0400

    changed fretboard looks

--- lib/notes/fretboard.dart
+++ lib/notes/fretboard.dart
@@ -13,14 +13,13 @@ class FretboardWidget extends StatelessWidget {
         child: Column(
           children: notes
               .map((row) => Row(
-            children:
-            row.map((note) => NoteWidget(note: note)).toList(),
-          ))
+                    children:
+                        row.map((note) => NoteWidget(note: note)).toList(),
+                  ))
               .toList(),
         ));
   }
 }
-
 
 class NoteWidget extends StatelessWidget {
   final Note note;
@@ -32,54 +31,58 @@ class NoteWidget extends StatelessWidget {
     return Expanded(
       child: Container(
         decoration: BoxDecoration(
-          // color: note.isActive ? Colors.yellowAccent : Colors.grey[100],
+            // color: note.isActive ? Colors.yellowAccent : Colors.grey[100],
             border: Border(
-                right: BorderSide(
-                    color: note.first ? Colors.red : Colors.black,
-                    width: note.first ? 10 : 2),
-                bottom: BorderSide())),
+          right: BorderSide(
+              color: note.first ? Colors.brown : Colors.brown,
+              width: note.first ? 8 : 1.5), /* bottom: BorderSide()*/
+        )),
         alignment: Alignment.center,
         height: 50,
         child: Row(
           children: [
             !note.first
                 ? Expanded(
-                child: Column(
-                  children: [
-                    Expanded(child: Container()),
-                    Container(
-                      color: Colors.orange,
-                      height: 1,
-                    ),
-                    Expanded(child: Container())
-                  ],
-                ))
+                    child: Column(
+                    children: [
+                      Expanded(child: Container()),
+                      Container(
+                        color: Colors.black,
+                        height: 1,
+                      ),
+                      Expanded(child: Container())
+                    ],
+                  ))
                 : Container(),
             Expanded(
                 child: Container(
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 0),
-                  decoration: BoxDecoration(
-                      borderRadius: note.first ? null : BorderRadius.circular(40),
-                      color: note.active ? Colors.redAccent : Colors.orange[100]),
-                  alignment: Alignment.center,
-                  child: Text(
-                    note.name ?? "err",
-                    style:
+              margin: EdgeInsets.symmetric(
+                  vertical: 10, horizontal: note.first ? 10 : 0),
+              decoration: BoxDecoration(
+                  borderRadius: note.first
+                      ? BorderRadius.circular(10)
+                      : BorderRadius.circular(40),
+                  color:
+                      note.active ? Colors.deepOrangeAccent : Colors.grey[300]),
+              alignment: Alignment.center,
+              child: Text(
+                note.name ?? "err",
+                style:
                     TextStyle(color: note.active ? Colors.white : Colors.black),
-                  ),
-                )),
+              ),
+            )),
             !note.first
                 ? Expanded(
-                child: Column(
-                  children: [
-                    Expanded(child: Container()),
-                    Container(
-                      color: Colors.orange,
-                      height: 1,
-                    ),
-                    Expanded(child: Container())
-                  ],
-                ))
+                    child: Column(
+                    children: [
+                      Expanded(child: Container()),
+                      Container(
+                        color: Colors.black,
+                        height: 1,
+                      ),
+                      Expanded(child: Container())
+                    ],
+                  ))
                 : Container(),
           ],
         ),
